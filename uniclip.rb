cf962ac4173e40f6c85a262c88187a78c2acbb7f
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-20 20:06:42 +0400

    Brew formula update for uniclip version v1.0.4

--- uniclip.rb
+++ uniclip.rb
@@ -2,24 +2,24 @@
 class Uniclip < Formula
   desc "Cross-platform shared clipboard"
   homepage "https://github.com/quackduck/uniclip"
-  version "1.0.3"
+  version "1.0.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/quackduck/uniclip/releases/download/v1.0.3/uniclip_1.0.3_Darwin_x86_64.tar.gz"
-    sha256 "73092d42fde81e4e22cf04fc51113a8abd55d07ff6d06764eb8676bedae312b3"
+    url "https://github.com/quackduck/uniclip/releases/download/v1.0.4/uniclip_1.0.4_Darwin_x86_64.tar.gz"
+    sha256 "cef02393a31871a857bc2b9407a533dade787cadfb17108da6cf0d7c655f9069"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/quackduck/uniclip/releases/download/v1.0.3/uniclip_1.0.3_Linux_x86_64.tar.gz"
-      sha256 "f108a5e03a432e17dbfb88d4c511ba553002d7d52a33b0723b2c36aca388f305"
+      url "https://github.com/quackduck/uniclip/releases/download/v1.0.4/uniclip_1.0.4_Linux_x86_64.tar.gz"
+      sha256 "242636fb7e4d648889e992d0b0b8c3d40f39b2e29db8146ec0013dd6dc032435"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/quackduck/uniclip/releases/download/v1.0.3/uniclip_1.0.3_Linux_arm64.tar.gz"
-        sha256 "8c1a167450a7751045af24d7d292fee3184179c9e2005f5cfd60b85e1bc9aea0"
+        url "https://github.com/quackduck/uniclip/releases/download/v1.0.4/uniclip_1.0.4_Linux_arm64.tar.gz"
+        sha256 "d9b5083a3aa91db101846f0ffed39c759802460e17def01affd532679b286b58"
       else
-        url "https://github.com/quackduck/uniclip/releases/download/v1.0.3/uniclip_1.0.3_Linux_armv6.tar.gz"
-        sha256 "0920203a3a989de35b91ad40c6db3b07658d1d4f44f0b42b6d07b5497a2cecb5"
+        url "https://github.com/quackduck/uniclip/releases/download/v1.0.4/uniclip_1.0.4_Linux_armv6.tar.gz"
+        sha256 "1b284c66692750f083e6d21570486f1e90dffdab27744859b80b220d2fb045ed"
       end
     end
   end
