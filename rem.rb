c17f3a4799e7f3ac5762b98b16c00266bc3eb3ad
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-16 09:57:59 +0400

    Brew formula update for rem version v2.0.0

--- rem.rb
+++ rem.rb
@@ -5,24 +5,24 @@
 class Rem < Formula
   desc "Rem is a CLI Trash which makes it ridiculously easy to recover them."
   homepage "https://github.com/quackduck/rem"
-  version "1.0.5"
+  version "2.0.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/quackduck/rem/releases/download/v1.0.5/rem_1.0.5_Darwin_x86_64.tar.gz"
-    sha256 "516ef0214547cdf903ebb9f2d8a872f06501b2ac7c3bab46b9c37f101a27d98d"
+    url "https://github.com/quackduck/rem/releases/download/v2.0.0/rem_2.0.0_Darwin_x86_64.tar.gz"
+    sha256 "d170c755dc23769a84d0b5d99995604c0f7c8310184eadf2e8e8b026562e96d9"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/quackduck/rem/releases/download/v1.0.5/rem_1.0.5_Linux_x86_64.tar.gz"
-    sha256 "1f96099e778a8bbfd40259b4f9140a8db68497e0dec9129da79d823a957489e3"
+    url "https://github.com/quackduck/rem/releases/download/v2.0.0/rem_2.0.0_Linux_x86_64.tar.gz"
+    sha256 "db0601db8bed2e4b694b8f839799a463d172719ec4d9a25220ada0ac5fe53dc3"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/quackduck/rem/releases/download/v1.0.5/rem_1.0.5_Linux_armv6.tar.gz"
-    sha256 "d6920fb4e3e6e82eb5a28727ec7471ff34070d12278d4a763dc96d1356542f12"
+    url "https://github.com/quackduck/rem/releases/download/v2.0.0/rem_2.0.0_Linux_armv6.tar.gz"
+    sha256 "9cd547bf9447bfea76fcdf1915156dda9d710cd78385832f6dfdffd598d9ec08"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/quackduck/rem/releases/download/v1.0.5/rem_1.0.5_Linux_arm64.tar.gz"
-    sha256 "721bc14b92f762f58a297f10e28fc233f35937ca5ead553e502537a61c2b4242"
+    url "https://github.com/quackduck/rem/releases/download/v2.0.0/rem_2.0.0_Linux_arm64.tar.gz"
+    sha256 "977947762136ce93ac2b8e3dea458c87b91076979c3ea56228953eee2fd16b19"
   end
 
   def install
