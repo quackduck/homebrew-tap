f2b0d4a02d196b3a7c95ae4f2fc9525fdf2b5e43
goreleaserbot <goreleaser@carlosbecker.com>
2021-12-20 13:28:14 -0800

    Brew formula update for rem version v2.3.5

--- rem.rb
+++ rem.rb
@@ -5,27 +5,51 @@
 class Rem < Formula
   desc "Rem is a CLI Trash which makes it ridiculously easy to recover them."
   homepage "https://github.com/quackduck/rem"
-  version "2.3.2"
-  bottle :unneeded
+  version "2.3.5"
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/quackduck/rem/releases/download/v2.3.2/rem_2.3.2_Darwin_x86_64.tar.gz"
-    sha256 "80cbfbd7e9cad6740bdefdfe88dd299f9b0484e16065cc476ebb975df2e5fc86"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/quackduck/rem/releases/download/v2.3.2/rem_2.3.2_Linux_x86_64.tar.gz"
-    sha256 "8fd9877c565b42283913cce7a32bac97b7d64706962f0c9db38d58bc751ae314"
-  end
-  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/quackduck/rem/releases/download/v2.3.2/rem_2.3.2_Linux_armv6.tar.gz"
-    sha256 "cfa701a66fb5c80072f076b279232c263aa41cbb0fa2cfd8ec641e56e0c4d5aa"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/quackduck/rem/releases/download/v2.3.2/rem_2.3.2_Linux_arm64.tar.gz"
-    sha256 "4eb4436611f1261f31ea1044a77acfee364f2a109c8ef417ebb0887f27b7a42e"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/quackduck/rem/releases/download/v2.3.5/rem_2.3.5_Darwin_arm64.tar.gz"
+      sha256 "3d54e9a73c4ae01b1e527d01eab0cca579eb41a09ee84be64ff06893b70ec4b6"
+
+      def install
+        bin.install "rem"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/quackduck/rem/releases/download/v2.3.5/rem_2.3.5_Darwin_x86_64.tar.gz"
+      sha256 "e88170593303e6e44da3ba36840a256cafa1f8137706913977afba8daeb31f3c"
+
+      def install
+        bin.install "rem"
+      end
+    end
   end
 
-  def install
-    bin.install "rem"
+  on_linux do
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/quackduck/rem/releases/download/v2.3.5/rem_2.3.5_Linux_armv6.tar.gz"
+      sha256 "8350afd3a4642796ede331b0e3c498216951212f9477e131e70e46365a16d355"
+
+      def install
+        bin.install "rem"
+      end
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/quackduck/rem/releases/download/v2.3.5/rem_2.3.5_Linux_arm64.tar.gz"
+      sha256 "90979697b3493ef9129407d71a234b06c617f51d6c17d6287f38cbff4e3f3ff9"
+
+      def install
+        bin.install "rem"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/quackduck/rem/releases/download/v2.3.5/rem_2.3.5_Linux_x86_64.tar.gz"
+      sha256 "f99f8a2013de2acb481bf952cb9fdf460cad7b69394c20e47a3df1522147a095"
+
+      def install
+        bin.install "rem"
+      end
+    end
   end
 end
