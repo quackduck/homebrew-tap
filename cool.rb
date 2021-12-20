c0a50e8b60ad4f88597277bb56e852863d334c83
goreleaserbot <goreleaser@carlosbecker.com>
2021-12-20 13:42:07 -0800

    Brew formula update for cool version v0.3.4

--- cool.rb
+++ cool.rb
@@ -5,19 +5,27 @@
 class Cool < Formula
   desc "Never let kernel_task eat up CPU again."
   homepage "https://github.com/quackduck/cool"
-  version "0.3.3"
-  bottle :unneeded
+  version "0.3.4"
   depends_on :macos
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/quackduck/cool/releases/download/v0.3.3/cool_0.3.3_Darwin_x86_64.tar.gz"
-      sha256 "a1417c13b56d5fd32ecc865f3e537d50ae96d95d0a2ea1b642a3323d257f8c56"
-    end
-  end
+    if Hardware::CPU.arm?
+      url "https://github.com/quackduck/cool/releases/download/v0.3.4/cool_0.3.4_Darwin_arm64.tar.gz"
+      sha256 "5f44823be50aba3c51c8931ca35be3aa8a97775716961512f098a48b0fedf09b"
 
-  def install
-    bin.install "cool"
-    bin.install "smc"
+      def install
+        bin.install "cool"
+        bin.install "smc"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/quackduck/cool/releases/download/v0.3.4/cool_0.3.4_Darwin_x86_64.tar.gz"
+      sha256 "b9ed22c6a1f94fc1dee05c4ccc63cba0be7f63759dcc3aac8675f55c7074c0f4"
+
+      def install
+        bin.install "cool"
+        bin.install "smc"
+      end
+    end
   end
 end
