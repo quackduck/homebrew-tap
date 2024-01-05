0b6d528b2bd34095c6cf1fdb472460594297e2f8
Ishan Goel <igoel.mail@gmail.com>
2024-01-05 13:11:24 -0800

    fix homepage for chanch

--- chanch.rb
+++ chanch.rb
@@ -1,6 +1,6 @@
 class Chanch < Formula
   desc "macOS WiFi channel changer"
-  homepage "https://github.com/quackduck"
+  homepage "https://github.com/quackduck/chanch"
   url "https://github.com/quackduck/chanch/releases/download/v0.1.5/release.tar.gz"
   sha256 "e8295799c1ebbdd9bc56c21afc74fab0b7d71cffdc4999ee4bb58a4cfe50e136"
   license "Unlicense"
