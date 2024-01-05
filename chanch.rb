9f56ee520194a51fed0151b7d62b4cce867b7b06
Ishan Goel <igoel.mail@gmail.com>
2024-01-05 12:17:23 -0800

    Create chanch.rb

---
+++ chanch.rb
@@ -0,0 +1,17 @@
+class Chanch < Formula
+  desc "macOS WiFi channel changer"
+  homepage "https://github.com/quackduck"
+  url "https://github.com/quackduck/chanch/releases/download/v0.1.5/release.tar.gz"
+  sha256 "e8295799c1ebbdd9bc56c21afc74fab0b7d71cffdc4999ee4bb58a4cfe50e136"
+  license "Unlicense"
+
+  depends_on :macos
+  uses_from_macos "swift"
+
+  def install
+    system "swift", "build",
+        "--configuration", "release",
+        "--disable-sandbox"
+    bin.install './chanch'
+  end
+end
